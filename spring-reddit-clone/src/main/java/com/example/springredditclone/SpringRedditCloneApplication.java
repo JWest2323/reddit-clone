ec0c70f9fa9bf020b7375ec22da51c2505e91a52
JWest2323 <jwest23@ufl.edu>
2022-06-11 14:34:55 -0500

    regen init to correct artifact name

--- spring-reddit-clone/src/main/java/com/example/springredditclone/SpringRedditCloneApplication.java
+++ spring-reddit-clone/src/main/java/com/example/springredditclone/SpringRedditCloneApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.springredditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringRedditCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringRedditCloneApplication.class, args);
 	}
 
 }
